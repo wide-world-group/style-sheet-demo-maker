162ba80bcca8d4c2d52012d7154954c4946c0f52
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-21 22:43:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CSS Demo Maker
+
+WORK IN PROGRESS
+
+The idea is that this will take one or more style sheets in the CSS format and 
+generate one HTML file demonstrating each of the rules.
---
+++ test/styleops/CSSDemoMakerTest.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package styleops;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests of the CSSDemoMaker class.
+ * @author Alonso del Arte
+ */
+public class CSSDemoMakerTest {
+    
+    /**
+     * Test of main method, of class CSSDemoMaker.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        CSSDemoMaker.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
